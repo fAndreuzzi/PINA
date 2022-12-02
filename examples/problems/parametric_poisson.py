e1cd3b03372d06f53d902fd1bb2239de595dae27
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-12-03 00:08:48 +0100

    restore

--- examples/problems/parametric_poisson.py
+++ examples/problems/parametric_poisson.py
@@ -9,12 +9,12 @@ class ParametricPoisson(SpatialProblem, ParametricProblem):
 
     output_variables = ['u']
     spatial_domain = Span({'x': [-1, 1], 'y': [-1, 1]})
-    parameter_domain = Span({'a': [-1, 1], 'b': [-1, 1]})
+    parameter_domain = Span({'mu1': [-1, 1], 'mu2': [-1, 1]})
 
     def laplace_equation(input_, output_):
         force_term = torch.exp(
-                - 2*(input_.extract(['x']) - input_.extract(['a']))**2
-                - 2*(input_.extract(['y']) - input_.extract(['b']))**2)
+                - 2*(input_.extract(['x']) - input_.extract(['mu1']))**2
+                - 2*(input_.extract(['y']) - input_.extract(['mu2']))**2)
         return nabla(output_.extract(['u']), input_) - force_term
 
     def nil_dirichlet(input_, output_):
@@ -23,18 +23,18 @@ class ParametricPoisson(SpatialProblem, ParametricProblem):
 
     conditions = {
         'gamma1': Condition(
-            Span({'x': [-1, 1], 'y': 1, 'a': [-1, 1], 'b': [-1, 1]}),
+            Span({'x': [-1, 1], 'y': 1, 'mu1': [-1, 1], 'mu2': [-1, 1]}),
             nil_dirichlet),
         'gamma2': Condition(
-            Span({'x': [-1, 1], 'y': -1, 'a': [-1, 1], 'b': [-1, 1]}),
+            Span({'x': [-1, 1], 'y': -1, 'mu1': [-1, 1], 'mu2': [-1, 1]}),
             nil_dirichlet),
         'gamma3': Condition(
-            Span({'x': 1, 'y': [-1, 1], 'a': [-1, 1], 'b': [-1, 1]}),
+            Span({'x': 1, 'y': [-1, 1], 'mu1': [-1, 1], 'mu2': [-1, 1]}),
             nil_dirichlet),
         'gamma4': Condition(
-            Span({'x': -1, 'y': [-1, 1], 'a': [-1, 1], 'b': [-1, 1]}),
+            Span({'x': -1, 'y': [-1, 1], 'mu1': [-1, 1], 'mu2': [-1, 1]}),
             nil_dirichlet),
         'D': Condition(
-            Span({'x': [-1, 1], 'y': [-1, 1], 'a': [-1, 1], 'b': [-1, 1]}),
+            Span({'x': [-1, 1], 'y': [-1, 1], 'mu1': [-1, 1], 'mu2': [-1, 1]}),
             laplace_equation),
     }
