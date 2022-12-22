9ee6e1d19f73d8999950e7b2f9461e1054518560
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2022-12-22 23:10:49 +0100

    re-enable options

--- examples/run_poisson_deeponet.py
+++ examples/run_poisson_deeponet.py
@@ -15,7 +15,7 @@ import argparse
 import logging
 
 logging.basicConfig(
-    filename="poisson_deeponet.log", filemode='w', level=logging.INFO
+    filename="poisson_deeponet.log", filemode="w", level=logging.INFO
 )
 
 
@@ -27,87 +27,84 @@ class myFeature(torch.nn.Module):
     def __init__(self, label):
         super(myFeature, self).__init__()
 
+        if not isinstance(label, (tuple, list)):
+            label = [label]
         self._label = label
 
     def forward(self, x):
-        t = torch.sin(x.extract([self._label]) * torch.pi)
+        t = torch.sin(x.extract(self._label) * torch.pi)
         return LabelTensor(t, [f"sin({self._label})"])
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run PINA")
+
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("-s", "-save", action="store_true")
     group.add_argument("-l", "-load", action="store_true")
     parser.add_argument("id_run", help="number of run", type=int)
-    #parser.add_argument("--extra", help="extra features", action="store_true")
-    #parser.add_argument(
-    #    "--combos", help="DeepONet internal network combinations", type=str
-    #)
-    parser.add_argument(
-        "--aggregator", help="Aggregator for DeepONet", type=str
-    )
-    parser.add_argument("--reduction", help="Reduction for DeepONet", type=str)
-    #parser.add_argument(
-    #    "--hidden",
-    #    help="Number of variables in the hidden DeepONet layer",
-    #    type=int,
-    #)
-    #parser.add_argument(
-    #    "--layers", help="Structure of the DeepONet partial layers", type=str
-    #)
-    args = parser.parse_args()
 
-    #feat = [myFeature()] if args.extra else []
+    parser.add_argument("--nobias", action="store_true")
+
+    parser.add_argument(
+        "--combos",
+        help="DeepONet internal network combinations",
+        type=str,
+        required=True,
+    )
+    parser.add_argument("--extra", help="extra features", action="store_true")
+
+    parser.add_argument(
+        "--aggregator", help="Aggregator for DeepONet", type=str, default="*"
+    )
+    parser.add_argument(
+        "--reduction", help="Reduction for DeepONet", type=str, default="+"
+    )
+
+    parser.add_argument(
+        "--hidden",
+        help="Number of variables in the hidden DeepONet layer",
+        type=int,
+        required=True,
+    )
+    parser.add_argument(
+        "--layers",
+        help="Structure of the DeepONet partial layers",
+        type=str,
+        required=True,
+    )
+    args = parser.parse_args()
 
     poisson_problem = Poisson()
 
-    # hidden_layers = list(map(int, args.layers.split(",")))
-    # combos = tuple(map(lambda combo: combo.split("-"), args.combos.split(",")))
-    # check_combos(combos, poisson_problem.input_variables)
-    # networks = spawn_combo_networks(combos, hidden_layers, args.hidden,
-    #                Softplus, extra_features=feat)
+    combos = tuple(map(lambda combo: combo.split("-"), args.combos.split(",")))
+    check_combos(combos, poisson_problem.input_variables)
 
-    x = FeedForward(
-        layers=[],
-        input_variables=["x"],
-        output_variables=1,
-        extra_features=[myFeature("x")],
-        bias=False
+    networks = spawn_combo_networks(
+        combos=combos,
+        layers=list(map(int, args.layers.split(","))) if args.layers else [],
+        output_dimension=args.hidden,
+        func=Softplus,
+        extra_feature=(lambda combo: [myFeature(combo)]) if args.extra else None,
+        bias=not args.nobias,
     )
-    y = FeedForward(
-        layers=[],
-        input_variables=["y"],
-        output_variables=1,
-        extra_features=[myFeature("y")],
-        bias=False
-    )
-    networks = [x, y]
 
     model = ComboDeepONet(
         networks,
         poisson_problem.output_variables,
         aggregator=args.aggregator,
-        reduction="+",
+        reduction=args.reduction,
     )
 
-    pinn = PINN(poisson_problem, model, lr=0.03, regularizer=1e-5)
+    pinn = PINN(poisson_problem, model, lr=0.01, regularizer=1e-8)
     if args.s:
         pinn.span_pts(
             20, "grid", locations=["gamma1", "gamma2", "gamma3", "gamma4"]
         )
         pinn.span_pts(20, "grid", locations=["D"])
-        pinn.train(300, 100)
+        pinn.train(1.e-10, 100)
         pinn.save_state("pina.poisson_{}".format(args.id_run))
     else:
         pinn.load_state("pina.poisson_{}".format(args.id_run))
         plotter = Plotter()
         plotter.plot(pinn)
-
-    logging.info('Net 0')
-    logging.info(model._nets[0].model[0].weight)
-    logging.info(model._nets[0].model[0].bias)
-
-    logging.info('Net 1')
-    logging.info(model._nets[1].model[0].weight)
-    logging.info(model._nets[1].model[0].bias)
